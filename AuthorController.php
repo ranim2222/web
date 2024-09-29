4885dadec2669a91dcad54f03beb17d900d11596
ranim2222 <159891610+ranim2222@users.noreply.github.com>
2024-09-29 02:16:28 -1200

    AuthorController.php

---
+++ AuthorController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+   #[Route('/showAuthor/{name}', name: 'app_showAuthor')]
+    public function showAuthor($name)
+    {
+        return $this->render('author/show.html.twig', ['n' => $name]);
+    }
+
+    #[Route('/showlist', name: 'app_showlist')]
+    public function listAuthors()
+    {
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/william.jpg','username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+            array('id' => 3, 'picture' => '/images/Taha-Hussein.jpg','username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
+            );  
+        return $this->render('author/list.html.twig', ['authors' => $authors]);
+          
+    }
+    
+    
+
+
+}
